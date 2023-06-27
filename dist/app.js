db6d7eb594b8fc71d5a425280af79cdeecdae400
Igor Parizi <100965139+pariziigor@users.noreply.github.com>
2023-06-27 16:08:36 -0300

    First comments typescript

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,22 @@
-"use strict";
-console.log('Hello World');
+export const comments = [
+    {
+        id: 1,
+        postId: 1,
+        email: 'teste@teste.com',
+        body: 'teste teste teste'
+    },
+    {
+        id: 2,
+        postId: 2,
+        email: 'teste@teste.com',
+        body: 'teste teste teste'
+    },
+    {
+        id: 3,
+        postId: 3,
+        email: 'teste@teste.com',
+        body: 'teste teste teste'
+    }
+];
+console.log(comments);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,36 @@
-console.log('Hello World');
+export interface Post {
+    id: number;
+    title: string;
+    imageUrl: string;
+    body: string;
+}
+
+export interface Comment {
+    id: number;
+    postId: number;
+    email: string;
+    body: string;
+}
+
+export const comments: Comment[] = [
+    {
+        id: 1,
+        postId: 1,
+        email: 'teste@teste.com',
+        body: 'teste teste teste'
+    },
+    {
+        id: 2,
+        postId: 2,
+        email: 'teste@teste.com',
+        body: 'teste teste teste'
+    },
+    {
+        id: 3,
+        postId: 3,
+        email: 'teste@teste.com',
+        body: 'teste teste teste'
+    }
+];
+
+console.log(comments);
