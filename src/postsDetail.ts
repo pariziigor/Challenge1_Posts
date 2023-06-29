03fa6e016dac6d08e80ce66ff94541b2e9369f95
Igor Parizi <100965139+pariziigor@users.noreply.github.com>
2023-06-29 17:39:05 -0300

    Added class for CSS

--- src/postsDetail.ts
+++ src/postsDetail.ts
@@ -18,35 +18,43 @@ export function showPostDetailsPage(postId: number) {
     const postDetails = document.createElement("div");
     postsElement.innerHTML = "";
     postsElement.appendChild(postDetails);
+    postDetails.classList.add("details-div");
 
     const image = document.createElement("img");
     image.src = post.imageUrl;
     postDetails.appendChild(image);
+    image.classList.add("details-img");
 
     const title = document.createElement("h2");
     title.textContent = post.title;
     postDetails.appendChild(title);
+    title.classList.add("details-title");
 
     const body = document.createElement("p");
     body.textContent = post.body;
     postDetails.appendChild(body);
+    body.classList.add("details-body");
 
     const comments = getCommentsByPostId(postId);
     if (comments.length > 0) {
         const commentsList = document.createElement("ul");
         postDetails.appendChild(commentsList);
+        commentsList.classList.add("comment-ul")
 
         comments.forEach((comment) => {
             const commentItem = document.createElement("li");
             commentsList.appendChild(commentItem);
+            commentItem.classList.add("comment-li");
 
             const email = document.createElement("strong");
             email.textContent = comment.email;
             commentItem.appendChild(email);
+            email.classList.add("comment-email");
 
             const commentBody = document.createElement("p");
             commentBody.textContent = comment.body;
             commentItem.appendChild(commentBody);
+            commentBody.classList.add("comment-body");
         });
     }
 
